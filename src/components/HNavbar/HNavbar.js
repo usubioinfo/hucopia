bb8aed177b23ba2580519d4d28f513e00534f8ce
MusicDev33 <smccowan33@gmail.com>
2021-11-15 11:38:36 -0700

    shrink navbar

--- src/components/HNavbar/HNavbar.js
+++ src/components/HNavbar/HNavbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import {Brand} from 'react-bootstrap';
 
 import './HNavbar.scss';
 import { env } from 'env.js';
@@ -34,12 +33,12 @@ class HNavbar extends React.Component {
 
     return (
       <div className="mx-5 mb-4 mt-3 nav-wrapper mx-auto">
-        <Navbar>
+        <Navbar className="justify-content-center">
           <Navbar.Brand className="h-brand">
             <b>HuCoPIA</b>
           </Navbar.Brand>
 
-          <Nav className="mr-auto">
+          <Nav className="">
             <Nav.Link href={`${env.BASE_URL}/home`} className={'home' === this.props.active ? active : className}>
               Home
             </Nav.Link>
