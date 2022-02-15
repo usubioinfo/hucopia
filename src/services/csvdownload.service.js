a5df6fe35558e926ef110615906ad1ca66a934a8
MusicDev33 <smccowan33@gmail.com>
2022-02-15 14:39:42 -0700

    Add csv download service

--- src/services/csvdownload.service.js
+++ src/services/csvdownload.service.js
@@ -1,1 +1,17 @@
 // This is going to be a general template for downloading as CSV.
+
+
+const downloadCsv = (data) => {
+  const csvPrefix = 'data:text/csv;charset=utf-8,';
+
+  let csvData = csvPrefix + data.map(row => row.join(',')).join('\n');
+  const encodedUri = encodeURI(csvData);
+  window.open(encodedUri);
+
+  let link = document.createElement('a');
+  link.setAttribute('href', encodedUri);
+  link.setAttributeNS('download', 'data.csv');
+  document.body.appendChild(link);
+
+  link.click();
+}
