7d4265639aec4e001008195df555c1c62770842a
Naveen Duhan <duhan27dec@gmail.com>
2022-02-15 16:21:11 -0700

    update

--- src/services/csvdownload.service.js
+++ src/services/csvdownload.service.js
@@ -3,15 +3,9 @@
 
 export const downloadCsv = (data) => {
   const csvPrefix = 'data:text/csv;charset=utf-8,';
-  console.log(data);
-  const escape = text =>
-    text.replace(/\\/g, "\\\\")
-        .replace(/\n/g, "\\n")
-        .replace(/,/g, "\\,")
-
-  let escaped_array = data.map(fields => fields.map(escape));
-  let csvData = csvPrefix + escaped_array.map(row => row.join(', ')).join('\n ');
-
+  
+  let csvData = csvPrefix + data.map(row => row.join(', ')).join('\n ');
+  console.log(csvData);
   const encodedUri = encodeURI(csvData);
   window.open(encodedUri);
 
