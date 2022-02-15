92c80128dab8ae3534aaaebb65d622cda670f0bd
Naveen Duhan <duhan27dec@gmail.com>
2022-02-15 16:13:54 -0700

    update

--- src/services/csvdownload.service.js
+++ src/services/csvdownload.service.js
@@ -4,7 +4,10 @@
 export const downloadCsv = (data) => {
   const csvPrefix = 'data:text/csv;charset=utf-8,';
   console.log(data);
-  let csvData = csvPrefix + data.map(row => row.join(', ')).join('\n ');
+  // let csvData = csvPrefix + data.map(row => row.join(', ')).join('\n ');
+  let csvData = csvPrefix + data.map(function(d){
+    return d.join();
+}).join('\n');
   const encodedUri = encodeURI(csvData);
   window.open(encodedUri);
 
