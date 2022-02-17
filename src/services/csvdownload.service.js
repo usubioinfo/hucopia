8ef936087df6a56d002a66aa293e2679b9fc897d
Naveen Duhan <duhan27dec@gmail.com>
2022-02-16 20:11:41 -0700

    update

--- src/services/csvdownload.service.js
+++ src/services/csvdownload.service.js
@@ -1,31 +1,36 @@
 // This is going to be a general template for downloading as CSV.
 
 
-export const downloadCsv = (data) => {
+export const downloadCsv = (data, tid) => {
   const csvPrefix = 'data:text/csv;charset=utf-8,';
+
+    const csvString = [
+      data.map(item => [
+        item.pathogen,
+        item.pathogenProtein,
+        item.isolate,
+        item.pLength,
+        item.gene,
+        item.hLength,
+        item.interactionType,
+        item.interactionCategory,
+        item.tissueExpression
+      ])
+    ]
+    .map(e => e.join(",")) 
+     .join("\n");
+     console.log(csvString);
   
-  const csvString = [
-    data.map(item => [
-      item.pathogen,
-      item.pathogenProtein,
-      item.isolate,
-      item.pLength,
-      item.gene,
-      item.hLength,
-      item.interactionType,
-      item.interactionCategory,
-      item.tissueExpression
-    ])
-  ];
 
-  console.log(csvString);
 
-  // let csvData = csvPrefix + data.map(row => row.join(', ')).join('\n ');
-  let csvData = csvPrefix + data.map(function(d){
-    return JSON.stringify(Object.values(d));
-})
-.join('\n') 
-.replace(/(^\[)|(\]$)/mg, '');
+ 
+
+  let csvData = csvPrefix + csvString
+//   let csvData = csvPrefix + data.map(function(d){
+//     return JSON.stringify(Object.values(d));
+// })
+// .join('\n') 
+// .replace(/(^\[)|(\]$)/mg, '');
 
   console.log(csvData);
   const encodedUri = encodeURI(csvData);
